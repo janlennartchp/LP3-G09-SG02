3f83481397a0b7a4670cd01006403d7c2f2fd3e9
josephg108j <joseph.alvarez@estudiante.ucsm.edu.pe>
2025-11-28 09:48:11 -0500

    Update CalculadoraDePrecios.java

--- informe 11/ejercicio2/strategy/CalculadoraDePrecios.java
+++ informe 11/ejercicio2/strategy/CalculadoraDePrecios.java
@@ -1,7 +1,16 @@
 package strategy;
+
 import java.util.List;
-public class CalculadoraDePrecios{
-    private Strategy strategy;
-    public void setStrategy(Strategy s){ strategy=s; }
-    public double calcularPrecio(List<Producto> p){ return strategy.aplicarDescuento(p);}
+
+public class CalculadoraDePrecios {
+
+    private Strategy estrategia;
+
+    public void setStrategy(Strategy estrategia) {
+        this.estrategia = estrategia;
+    }
+
+    public double calcularPrecio(List<Producto> productos) {
+        return estrategia.aplicarDescuento(productos);
+    }
 }
