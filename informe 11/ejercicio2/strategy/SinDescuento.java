27808b65eed43a9667338ab417b3a48ba360a245
josephg108j <joseph.alvarez@estudiante.ucsm.edu.pe>
2025-11-28 09:49:19 -0500

    Update SinDescuento.java

--- informe 11/ejercicio2/strategy/SinDescuento.java
+++ informe 11/ejercicio2/strategy/SinDescuento.java
@@ -1,5 +1,11 @@
 package strategy;
+
 import java.util.List;
-public class SinDescuento implements Strategy{
-    public double aplicarDescuento(List<Producto> p){ return p.stream().mapToDouble(Producto::getPrecio).sum();}
+
+public class SinDescuento implements Strategy {
+
+    @Override
+    public double aplicarDescuento(List<Producto> productos) {
+        return productos.stream().mapToDouble(Producto::getPrecio).sum();
+    }
 }
