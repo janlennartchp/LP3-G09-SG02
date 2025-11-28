4bffee219be5a02b5395d41b1cbe8c393319e821
josephg108j <joseph.alvarez@estudiante.ucsm.edu.pe>
2025-11-28 10:02:50 -0500

    Update Subject.java

--- informe 11/ejercicio3/observer/Subject.java
+++ informe 11/ejercicio3/observer/Subject.java
@@ -1,8 +1,22 @@
 package observer;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Subject {
     private List<Observer> observers = new ArrayList<>();
-    public void attach(Observer o){ observers.add(o);}
-    public void detach(Observer o){ observers.remove(o);}
-    public void notifyObservers(String m){ for(Observer o:observers)o.update(m);}
+
+    public void attach(Observer o) {
+        observers.add(o);
+    }
+
+    public void detach(Observer o) {
+        observers.remove(o);
+    }
+
+    public void notifyObservers(String mensaje) {
+        for (Observer o : observers) {
+            o.update(mensaje);
+        }
+    }
 }
