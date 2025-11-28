5ab39d7181dd6cf56a49b30fe819b15dcbbc4d07
josephg108j <joseph.alvarez@estudiante.ucsm.edu.pe>
2025-11-28 09:45:49 -0500

    Update Subject.java

--- informe 11/ejercicio1/observer/Subject.java
+++ informe 11/ejercicio1/observer/Subject.java
@@ -1,8 +1,26 @@
 package observer;
-import java.util.*;
-public class Subject {
-    private List<Observer> observers = new ArrayList<>();
-    public void attach(Observer o){ observers.add(o);}
-    public void detach(Observer o){ observers.remove(o);}
-    public void notifyObservers(String m){ for(Observer o:observers)o.update(m);}
+
+public class MainObserver {
+
+    public static void main(String[] args) {
+
+        Notificacion sistema = new Notificacion();
+
+        Usuario u1 = new Usuario("Carlos");
+        Usuario u2 = new Usuario("María");
+        Usuario u3 = new Usuario("José");
+
+        sistema.attach(u1);
+        sistema.attach(u2);
+        sistema.attach(u3);
+
+        System.out.println("Enviando Notificación 1 ");
+        sistema.nuevoEvento("Nueva promoción disponible!");
+
+        System.out.println(" María se desuscribe ");
+        sistema.detach(u2);
+
+        System.out.println("Enviando Notificación 2 ");
+        sistema.nuevoEvento("Producto en oferta!");
+    }
 }
