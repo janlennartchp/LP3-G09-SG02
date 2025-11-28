2c5ecca6390f1410d508d52ccad35de9b63b56ab
josephg108j <joseph.alvarez@estudiante.ucsm.edu.pe>
2025-11-28 10:03:50 -0500

    Update DescuentoPorcentualAcumulado.java

--- informe 11/ejercicio3/strategy/DescuentoPorcentualAcumulado.java
+++ informe 11/ejercicio3/strategy/DescuentoPorcentualAcumulado.java
@@ -1,13 +1,24 @@
 package strategy;
-import java.util.*;
-public class DescuentoPorcentualAcumulado implements Strategy{
-    public double aplicarDescuento(List<Producto> p){
-        if(p.size()>=3){
-            Producto barato=p.stream().min(Comparator.comparingDouble(Producto::getPrecio)).get();
-            double desc=barato.getPrecio()*0.50;
-            double total=p.stream().mapToDouble(Producto::getPrecio).sum();
-            return total-desc;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class DescuentoPorcentualAcumulado implements Strategy {
+
+    @Override
+    public double aplicarDescuento(List<Producto> productos) {
+        if (productos.size() >= 3) {
+            Producto masBarato = productos.stream()
+                                          .min(Comparator.comparingDouble(Producto::getPrecio))
+                                          .get();
+
+            double descuento = masBarato.getPrecio() * 0.50;
+
+            double total = productos.stream().mapToDouble(Producto::getPrecio).sum();
+
+            return total - descuento;
         }
-        return p.stream().mapToDouble(Producto::getPrecio).sum();
+
+        return productos.stream().mapToDouble(Producto::getPrecio).sum();
     }
 }
