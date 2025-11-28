7d6c7f71636efcabb3a835f389a6016dcea23127
josephg108j <joseph.alvarez@estudiante.ucsm.edu.pe>
2025-11-28 09:49:08 -0500

    Update Producto.java

--- informe 11/ejercicio2/strategy/Producto.java
+++ informe 11/ejercicio2/strategy/Producto.java
@@ -1,7 +1,19 @@
 package strategy;
-public class Producto{
-    private String nombre; private double precio;
-    public Producto(String n,double p){ nombre=n; precio=p;}
-    public String getNombre(){ return nombre;}
-    public double getPrecio(){ return precio;}
+
+public class Producto {
+    private String nombre;
+    private double precio;
+
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
