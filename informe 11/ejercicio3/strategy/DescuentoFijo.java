b452b4ae9c485bb49e1856d320a9e83a8851e0ed
josephg108j <joseph.alvarez@estudiante.ucsm.edu.pe>
2025-11-28 10:03:27 -0500

    Update DescuentoFijo.java

--- informe 11/ejercicio3/strategy/DescuentoFijo.java
+++ informe 11/ejercicio3/strategy/DescuentoFijo.java
@@ -1,8 +1,12 @@
 package strategy;
+
 import java.util.List;
-public class DescuentoFijo implements Strategy{
-    public double aplicarDescuento(List<Producto> p){
-        double total=p.stream().mapToDouble(Producto::getPrecio).sum();
-        return total*0.90;
+
+public class DescuentoFijo implements Strategy {
+
+    @Override
+    public double aplicarDescuento(List<Producto> productos) {
+        double total = productos.stream().mapToDouble(Producto::getPrecio).sum();
+        return total * 0.90;
     }
 }
