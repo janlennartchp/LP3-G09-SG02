2d15b3cf84743759217fc24df2637c4e2fbf87aa
josephg108j <joseph.alvarez@estudiante.ucsm.edu.pe>
2025-11-28 10:02:17 -0500

    Update MainObserver.java

--- informe 11/ejercicio3/observer/MainObserver.java
+++ informe 11/ejercicio3/observer/MainObserver.java
@@ -1,12 +1,26 @@
 package observer;
-public class MainObserver{
-    public static void main(String[] args){
-        Notificacion sistema=new Notificacion();
-        Usuario u1=new Usuario("Carlos");
-        Usuario u2=new Usuario("María");
-        sistema.attach(u1); sistema.attach(u2);
-        sistema.nuevoEvento("¡Nueva promoción disponible!");
+
+public class MainObserver {
+
+    public static void main(String[] args) {
+
+        Notificacion sistema = new Notificacion();
+
+        Usuario u1 = new Usuario("Carlos");
+        Usuario u2 = new Usuario("María");
+        Usuario u3 = new Usuario("José");
+
+        sistema.attach(u1);
+        sistema.attach(u2);
+        sistema.attach(u3);
+
+        System.out.println(" Enviando Notificación 1 ");
+        sistema.nuevoEvento("Nueva promoción disponible!");
+
+        System.out.println("María se desuscribe ");
         sistema.detach(u2);
-        sistema.nuevoEvento("¡Producto nuevo agregado!");
+
+        System.out.println( "Enviando Notificación 2 ");
+        sistema.nuevoEvento("Producto en oferta!");
     }
 }
